26b3a83915ac1ac3c86bfaf68c385a9062011732
Andriniaina Fabien Rakotomanana <fabrakoto0987@gmail.com>
2023-05-31 20:14:03 +0300

    refactor

--- vet/models/poste.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-class Poste(models.Model):
-    designation = models.CharField(max_length=255)
--- vet/models/race.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Race(models.Model):
-    designation=models.CharField(max_length=255)
